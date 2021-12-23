883410259185ab2018cae1cdba4635edeb6cb05d
nandreu2000 <nandreu2000@gmail.com>
2021-12-22 21:11:57 -0300

    Primer-arreglo

---
+++ src/MiModulo.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const MiModulo = () => {
+
+    return (
+        <div>
+            <h2>Hola Primer Trabajo Practico en React Js!</h2>
+            <hr/>
+            <p>Aca iria un contenido</p>
+        </div>
+    )
+}
+
+export default MiModulo
